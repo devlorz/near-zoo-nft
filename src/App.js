b7345f0885b0885ae929eea4f5c03fb23886cf02
Nutti Saelor <leelorz6@gmail.com>
2022-04-28 21:13:47 +0700

    Implement frontend

--- src/App.js
+++ src/App.js
@@ -1,197 +1,135 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import { login, logout } from './utils'
-import './global.css'
+import { utils } from "near-api-js";
+import React from "react";
+import "regenerator-runtime/runtime";
+import { v4 as uuid4 } from "uuid";
+import "./global.css";
+import { login, logout } from "./utils";
 
-import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+const GAS = 300000000000000;
+const ONE_NEAR = utils.format.parseNearAmount("1");
+
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+const animalPics = [
+  "https://bafybeihpxmpxynscvhr6qe6brakdhsuf4khoqf5vmmcdfxy5gardunmlce.ipfs.nftstorage.link",
+  "https://bafybeiaq7ac5pd2ngmaec2fuqfeznnqutojaaoeuyntp3u2pvxks2qvi3e.ipfs.nftstorage.link",
+  "https://bafybeiakjlzhrwbdxjklyskyn6k5dvdrjrwteys2k3gqbhleyl33nsxjha.ipfs.nftstorage.link",
+  "https://bafybeiehj4hmkrlahj545pg55ev2sgshs44erobd225nvlhwzgcbfuonbe.ipfs.nftstorage.link",
+  "https://bafybeicwytige6acrrtjt7d3v5f7cfsdqac63hfh3elnlnmjxogbch3jda.ipfs.nftstorage.link",
+];
 
 export default function App() {
-  // use React Hooks to store greeting in component state
-  const [greeting, set_greeting] = React.useState()
+  const [nfts, setNfts] = React.useState([]);
 
-  // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
-  // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false)
+  React.useEffect(() => {
+    if (window.walletConnection.isSignedIn()) {
+      window.contract
+        .nft_tokens_for_owner({ account_id: window.accountId })
+        .then((res) => {
+          setNfts(res);
+        });
+    }
+  }, []);
 
-  // The useEffect hook can be used to fire side-effects during render
-  // Learn more: https://reactjs.org/docs/hooks-intro.html
-  React.useEffect(
-    () => {
-      // in this case, we only care to query the contract when signed in
-      if (window.walletConnection.isSignedIn()) {
-
-        // window.contract is set by initContract in index.js
-        window.contract.get_greeting({ account_id: window.accountId })
-          .then(greetingFromContract => {
-            set_greeting(greetingFromContract)
-          })
-      }
-    },
-
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
-    []
-  )
-
-  // if not signed in, return early with sign-in prompt
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>Welcome to NEAR!</h1>
-        <p>
-          To make use of the NEAR blockchain, you need to sign in. The button
-          below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
-        <p>
-          Go ahead and click the button below to try it out:
-        </p>
-        <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
+        <h1>Alpha Pass Mint</h1>
+        <p style={{ textAlign: "center", marginTop: "2.5em" }}>
           <button onClick={login}>Sign in</button>
         </p>
       </main>
-    )
+    );
   }
 
+  const mint = async () => {
+    const token_owner_id = window.walletConnection.account().accountId;
+    try {
+      const token_id = await window.contract.nft_total_supply();
+      console.log(token_id);
+      // const token_id = uuid4();
+      const media = animalPics.random();
+      const token_metadata = {
+        title: `Zoo NFT #${token_id}`,
+        description:
+          "Ukrainian zoos are in severe need of help now and will be in need after the war ends. There are no visitors in the zoos, which results in no budgeting. All the NFT mint funds will be donated to Ukrainian zoo.",
+        media,
+        copies: 1,
+      };
+      setButtonDisabled(true);
+
+      await window.contract.nft_mint(
+        {
+          token_id,
+          token_owner_id,
+          token_metadata,
+        },
+        GAS,
+        ONE_NEAR
+      );
+    } catch (e) {
+      alert(
+        "Something went wrong! " +
+          "Maybe you need to sign out and back in? " +
+          "Check your browser console for more info."
+      );
+      throw e;
+    } finally {
+      setButtonDisabled(false);
+    }
+
+    setNfts([...nfts, { token_id, token_owner_id, token_metadata }]);
+  };
+
   return (
-    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
     <>
-      <button className="link" style={{ float: 'right' }} onClick={logout}>
+      <button className="link" style={{ float: "right" }} onClick={logout}>
         Sign out
       </button>
       <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)'
-            }}
-          >
-            {greeting}
-          </label>
-          {' '/* React trims whitespace around tags; insert literal space character when needed */}
-          {window.accountId}!
-        </h1>
-        <form onSubmit={async event => {
-          event.preventDefault()
-
-          // get elements from the form using their id attribute
-          const { fieldset, greeting } = event.target.elements
-
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-          const newGreeting = greeting.value
-
-          // disable the form while the value gets updated on-chain
-          fieldset.disabled = true
-
-          try {
-            // make an update call to the smart contract
-            await window.contract.set_greeting({
-              // pass the value that the user entered in the greeting field
-              message: newGreeting
-            })
-          } catch (e) {
-            alert(
-              'Something went wrong! ' +
-              'Maybe you need to sign out and back in? ' +
-              'Check your browser console for more info.'
-            )
-            throw e
-          } finally {
-            // re-enable the form, whether the call succeeded or failed
-            fieldset.disabled = false
-          }
-
-          // update local `greeting` variable to match persisted value
-          set_greeting(newGreeting)
-
-          // show Notification
-          setShowNotification(true)
-
-          // remove Notification again after css animation completes
-          // this allows it to be shown again next time the form is submitted
-          setTimeout(() => {
-            setShowNotification(false)
-          }, 11000)
-        }}>
-          <fieldset id="fieldset">
-            <label
-              htmlFor="greeting"
+        <h1>{"Alpha Pass Mint"}</h1>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            maxWidth: "100%",
+          }}
+        >
+          {nfts.map((nft) => (
+            <div
+              key={nft.token_id}
               style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
+                marginRight: 5,
+                flexGrow: 1,
+                flexBasis: "50%",
               }}
             >
-              Change greeting
-            </label>
-            <div style={{ display: 'flex' }}>
-              <input
-                autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
-                style={{ flex: 1 }}
-              />
-              <button
-                disabled={buttonDisabled}
-                style={{ borderRadius: '0 5px 5px 0' }}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginBottom: 10,
+                }}
               >
-                Save
-              </button>
+                {nft.metadata.title}
+              </div>
+              <img style={{ width: "100%" }} src={nft.metadata.media} />
             </div>
-          </fieldset>
-        </form>
-        <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
-        </p>
-        <ol>
-          <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>get_greeting</code> and <code>set_greeting</code> being called on <code>contract</code>. What's this?
-          </li>
-          <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/develop/contracts/overview">smart contract</a>.</li>
-          <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
-        </ol>
-        <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
-      </main>
-      {showNotification && <Notification />}
-    </>
-  )
-}
+          ))}
+        </div>
 
-// this component gets rendered by App after the form is submitted
-function Notification() {
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
-  return (
-    <aside>
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
-        {window.accountId}
-      </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
-      called method: 'set_greeting' in contract:
-      {' '}
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
-        {window.contract.contractId}
-      </a>
-      <footer>
-        <div>✔ Succeeded</div>
-        <div>Just now</div>
-      </footer>
-    </aside>
-  )
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <p style={{ textAlign: "center", marginTop: "2.5em" }}>
+            <button disabled={buttonDisabled} onClick={mint}>
+              Mint
+            </button>
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }
